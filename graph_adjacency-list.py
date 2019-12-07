d67473ba8ee8c253d3d60451138ea5e47f7c0312
b8004889 <a.lam11@newcastle.ac.uk>
2019-12-07 19:08:51 +0000

    Test return statements. Added comments.

--- graph_adjacency-list.py
+++ graph_adjacency-list.py
@@ -7,27 +7,28 @@ class Vertex:
 	def add_neighbor(self, v):
 		if v not in self.neighbors:
 			self.neighbors.append(v)
-			self.neighbors.sort()
+			self.neighbors.sort() # Python list.sort() sorts alphabetically
 
 class Graph:
 	vertices = {}
-	
+
 	def add_vertex(self, vertex):
 		if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
+			# comparing if this vertex is same as one in class Vertex
 			self.vertices[vertex.name] = vertex
-			return True
+			return "I'm in add_vertex - previously no vertices"
 		else:
-			return False
+			return "I didn't work 1 - already vertices"
 	
 	def add_edge(self, u, v):
 		if u in self.vertices and v in self.vertices:
 			# my YouTube video shows a silly for loop here, but this is a much faster way to do it
 			self.vertices[u].add_neighbor(v)
 			self.vertices[v].add_neighbor(u)
-			return True
+			return "I'm in add_edge"
 		else:
-			return False
-			
+			return "I didn't work 2"
+
 	def print_graph(self):
 		for key in sorted(list(self.vertices.keys())):
 			print(key + str(self.vertices[key].neighbors))
@@ -38,10 +39,13 @@ a = Vertex('A')
 g.add_vertex(a)
 g.add_vertex(Vertex('B'))
 for i in range(ord('A'), ord('K')):
-	g.add_vertex(Vertex(chr(i)))
+	print(g.add_vertex(Vertex(chr(i))))
+
+for i in range(ord('A'), ord('K')):
+	print(g.add_vertex(Vertex(chr(i))))
 
 edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
 for edge in edges:
-	g.add_edge(edge[:1], edge[1:])
+	print(g.add_edge(edge[:1], edge[1:])) # Splitting each edge from everything up to first element.
 
 g.print_graph()
