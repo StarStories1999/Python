2640c4d8944137d3e19514bd64a5ea3dd4130e5a
b8004889 <a.lam11@newcastle.ac.uk>
2019-12-08 19:31:10 +0000

    Changed variable names: edge = link, vertex = node.

--- graph_adjacency-matrix.py
+++ graph_adjacency-matrix.py
@@ -1,49 +1,49 @@
-# implementation of an undirected graph using Adjacency Matrix, with weighted or unweighted edges
-class Vertex:
+# implementation of an undirected graph using Adjacency Matrix, with weighted or unweighted links
+class Node:
 	def __init__(self, n):
 		self.name = n
 
 class Graph:
-	vertices = {}
-	edges = []
-	edge_indices = {}
+	nodes = {}
+	links = []
+	link_indices = {}
 	
-	def add_vertex(self, vertex):
-		if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
-			self.vertices[vertex.name] = vertex
-			for row in self.edges:
+	def add_node(self, node):
+		if isinstance(node, Node) and node.name not in self.nodes:
+			self.nodes[node.name] = node
+			for row in self.links:
 				row.append(0)
-			self.edges.append([0] * (len(self.edges)+1))
-			self.edge_indices[vertex.name] = len(self.edge_indices)
+			self.links.append([0] * (len(self.links)+1))
+			self.link_indices[node.name] = len(self.link_indices)
 			return True
 		else:
 			return False
 	
-	def add_edge(self, u, v, weight=1):
-		if u in self.vertices and v in self.vertices:
-			self.edges[self.edge_indices[u]][self.edge_indices[v]] = weight
-			self.edges[self.edge_indices[v]][self.edge_indices[u]] = weight
+	def add_link(self, u, v, weight=1):
+		if u in self.nodes and v in self.nodes:
+			self.links[self.link_indices[u]][self.link_indices[v]] = weight
+			self.links[self.link_indices[v]][self.link_indices[u]] = weight
 			return True
 		else:
 			return False
 			
 	def print_graph(self):
-		for v, i in sorted(self.edge_indices.items()):
+		for v, i in sorted(self.link_indices.items()):
 			print(v + ' ', end='')
-			for j in range(len(self.edges)):
-				print(self.edges[i][j], end='')
+			for j in range(len(self.links)):
+				print(self.links[i][j], end='')
 			print(' ')    
 
 g = Graph()
-# print(str(len(g.vertices)))
-a = Vertex('A')
-g.add_vertex(a)
-g.add_vertex(Vertex('B'))
+# print(str(len(g.nodes)))
+a = Node('A')
+g.add_node(a)
+g.add_node(Node('B'))
 for i in range(ord('A'), ord('K')):
-	g.add_vertex(Vertex(chr(i)))
+	g.add_node(Node(chr(i)))
 
-edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
-for edge in edges:
-	g.add_edge(edge[:1], edge[1:])
+links = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
+for link in links:
+	g.add_link(link[:1], link[1:])
 
 g.print_graph()
