3a946db2d0e14fc6d52750cc6e707cc639e9f464
b8004889 <a.lam11@newcastle.ac.uk>
2019-12-08 19:23:05 +0000

    Changed variable names to 'node' and 'target node'.

--- graph_adjacency-list.py
+++ graph_adjacency-list.py
@@ -4,10 +4,10 @@ class Vertex:
 		self.name = n
 		self.neighbors = list()
 	
-	def add_neighbor(self, v):
-		if v not in self.neighbors:
-			self.neighbors.append(v)
-			self.neighbors.sort() # Python list.sort() sorts alphabetically if no params to sort otherwise.
+	def add_neighbor(self, target_node):
+		if target_node not in self.neighbors:
+			self.neighbors.append(target_node)
+			self.neighbors.sort()  # Python list.sort() sorts alphabetically if no params to sort otherwise.
 
 class Graph:
 	vertices = {}
@@ -20,32 +20,33 @@ class Graph:
 		else:
 			return "I didn't work 1 - already vertices"
 	
-	def add_edge(self, u, v):
-		if u in self.vertices and v in self.vertices:
+	def add_edge(self, node, target_node):
+		if node in self.vertices and target_node in self.vertices:
 			# my YouTube video shows a silly for loop here, but this is a much faster way to do it
-			self.vertices[u].add_neighbor(v)
-			self.vertices[v].add_neighbor(u)
+			self.vertices[node].add_neighbor(target_node)
+			self.vertices[target_node].add_neighbor(node)
 			return "I'm in add_edge"
 		else:
 			return "I didn't work 2"
 
 	def print_graph(self):
-		for key in sorted(list(self.vertices.keys())):
-			print(key + str(self.vertices[key].neighbors))
+		for key in (list(self.vertices.keys())):
+			print(key + (str(self.vertices[key].neighbors)))
 
 g = Graph()
 # print(str(len(g.vertices)))
-a = Vertex('A')
-g.add_vertex(a)
+g.add_vertex(Vertex('A'))
 g.add_vertex(Vertex('B'))
-for i in range(ord('A'), ord('K')):
-	print(g.add_vertex(Vertex(chr(i))))
 
 for i in range(ord('A'), ord('K')):
 	print(g.add_vertex(Vertex(chr(i))))
+
+# for i in range(ord('A'), ord('K')):
+# 	print((g.add_vertex(Vertex(str(i)))))  # Already added so returns False.
 
 edges = ['AB', 'AE', 'BF', 'CG', 'DE', 'DH', 'EH', 'FG', 'FI', 'FJ', 'GJ', 'HI']
 for edge in edges:
-	print(g.add_edge(edge[:1], edge[1:])) # Splitting each edge from everything up to first element.
+	mapping = g.add_edge(edge[:1], edge[1])
+	# print(sorted(mapping,key=len))                      # Splitting each edge from everything up to first element.
 
 g.print_graph()
